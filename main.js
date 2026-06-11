e039925ee66fa0ca138809b6353efda99fa14771
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-06-11 22:26:59 +0530

    initial

---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const dbConnection = require("./config/dbconnection.config");
+
+dbConnection();
+app.listen(process.env.port, () => {
+    console.log(`Server running on ${process.env.port}`);
+});
