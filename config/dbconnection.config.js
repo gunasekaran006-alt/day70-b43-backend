dfbd523b001bafe74368b5613a139250602afff7
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-06-25 23:57:35 +0530

    updates

--- config/dbconnection.config.js
+++ config/dbconnection.config.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const dbConnection = async () => {
-    try{
-        await mongoose.connect(process.env.atlasport);
+    try {
+        await mongoose.connect(process.env.dbport);
         console.log("Database Connected");
+
     }catch(err){
         console.log(err);
         process.exit(1);
--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const app = express();
 require("dotenv").config();
 
 const dbConnection = require("./config/dbconnection.config");
-
 dbConnection();
+
 app.listen(process.env.port, () => {
     console.log(`Server running on ${process.env.port}`);
 });
